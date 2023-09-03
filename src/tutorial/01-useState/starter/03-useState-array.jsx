a919f60132ecb7e6aacaed8172ae6b9e8b708ab1
thenatural86 <philip.smalls86@gmail.com>
2023-09-03 15:57:18 -0400

    buttons to remove and clear

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,32 @@
-const UseStateArray = () => {
-  return <h2>useState array example</h2>;
-};
+import { useState } from 'react'
+import { data } from '../../../data'
 
-export default UseStateArray;
+const UseStateArray = () => {
+  const [people, setPeople] = useState(data)
+
+  const removePerson = (id) => {
+    const newArray = people.filter((person) => person.id !== id)
+    setPeople(newArray)
+  }
+
+  const clearList = () => {
+    setPeople([])
+  }
+
+  return (
+    <div>
+      <h2>useState array example</h2>
+      {people.map((person) => {
+        const { id, name } = person
+        return (
+          <div key={id}>
+            {name} | <button onClick={() => removePerson(id)}>Remove</button>
+          </div>
+        )
+      })}
+      <button onClick={() => clearList()}>Clear List</button>
+    </div>
+  )
+}
+
+export default UseStateArray
