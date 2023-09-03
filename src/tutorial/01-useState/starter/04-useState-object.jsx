d2bfaee2a767eb71e26e547821296c4151d30ef4
thenatural86 <philip.smalls86@gmail.com>
2023-09-03 16:56:27 -0400

    objec state

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,32 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
-};
+  const [person, setPerson] = useState({
+    name: 'Luke',
+    age: 28,
+    hobby: 'Traveling',
+  })
 
-export default UseStateObject;
+  const displayPerson = () => {
+    setPerson({
+      name: 'Erin',
+      age: 24,
+      hobby: 'being with Philip',
+    })
+  }
+  const { name, age, hobby } = person
+  return (
+    <div>
+      <h2>Individual</h2>
+      <h5>{name}</h5>
+      <h5>{age}</h5>
+      <h5>{hobby}</h5>
+
+      <button className='btn' onClick={displayPerson}>
+        Change Now!
+      </button>
+    </div>
+  )
+}
+
+export default UseStateObject
