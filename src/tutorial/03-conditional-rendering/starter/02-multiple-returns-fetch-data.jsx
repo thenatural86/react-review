479fae9bc362f5940a15fdaeafc77a5450f8f6eb
thenatural86 <philip.smalls86@gmail.com>
2023-09-04 15:45:30 -0400

    render user

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,7 +1,38 @@
-import { useEffect, useState } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import { useEffect, useState } from 'react'
+const url = 'https://api.github.com/users/QuincyLarson'
 
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
-};
-export default MultipleReturnsFetchData;
+  const [isLoading, setIsLoading] = useState(true)
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url)
+        const user = await response.json()
+        setUser(user)
+      } catch (error) {
+        console.log(error)
+      }
+      setIsLoading(false)
+    }
+    fetchData()
+  }, [])
+
+  return (
+    <>
+      <h2>Fetch Data </h2>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          <img src={user.avatar_url} alt={user.name} />
+          <h2>{user.name}</h2>
+          <h4>Works{user.company}</h4>
+          <h5>{user.bio}</h5>
+        </div>
+      )}
+    </>
+  )
+}
+export default MultipleReturnsFetchData
