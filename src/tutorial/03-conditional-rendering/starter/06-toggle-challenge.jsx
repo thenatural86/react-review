7ed2ca6230cd2d803242b99072d25744ac1179b7
thenatural86 <philip.smalls86@gmail.com>
2023-09-05 16:11:09 -0400

    toggle button

--- src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,7 +1,20 @@
 import { useState } from 'react'
 
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>
+  const [isToggled, setIsToggled] = useState(false)
+
+  const toggleDiv = () => {
+    setIsToggled(!isToggled)
+  }
+  return (
+    <div>
+      <h2>toggle challenge</h2>
+      <section>{isToggled && <h4>Here I am!</h4>}</section>
+      <button type='button' className='btn' onClick={toggleDiv}>
+        Click Me!
+      </button>
+    </div>
+  )
 }
 
 export default ToggleChallenge
