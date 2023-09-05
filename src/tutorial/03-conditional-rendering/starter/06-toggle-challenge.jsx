eb114035097c7250e6f92a8abea397196b10a8ee
thenatural86 <philip.smalls86@gmail.com>
2023-09-05 16:06:54 -0400

    toggle

--- src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,7 @@
-const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
-};
+import { useState } from 'react'
 
-export default ToggleChallenge;
+const ToggleChallenge = () => {
+  return <h2>toggle challenge</h2>
+}
+
+export default ToggleChallenge
