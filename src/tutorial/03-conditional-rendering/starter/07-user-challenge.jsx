ad18b34f343cfbcb9eac633bb262becb803e7d0f
thenatural86 <philip.smalls86@gmail.com>
2023-09-06 09:50:01 -0400

    ternary rendering

--- src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,36 @@
-const UserChallenge = () => {
-  return <h2>user challenge</h2>;
-};
+import { useState } from 'react'
 
-export default UserChallenge;
+const UserChallenge = () => {
+  const [user, setUser] = useState(null)
+
+  const login = () => {
+    setUser({
+      name: 'xavier',
+    })
+  }
+  const logout = () => {
+    setUser(null)
+  }
+  return (
+    <>
+      <h2>user challenge</h2>
+      {user ? (
+        <div>
+          <h4>{`Hello there ${user.name}`}</h4>
+          <button type='button' className='btn' onClick={logout}>
+            Logout
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h4>{`Please login`}</h4>
+          <button type='button' className='btn' onClick={login}>
+            Login
+          </button>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default UserChallenge
