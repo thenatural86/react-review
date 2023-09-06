75d203ded5d39926125e52d9fc5fd06d98bed244
thenatural86 <philip.smalls86@gmail.com>
2023-09-06 10:41:00 -0400

    cleanup function

--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,23 @@
-const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
-};
+import { useState, useEffect } from 'react'
 
-export default CleanupFunction;
+const CleanupFunction = () => {
+  const [value, setValue] = useState(false)
+
+  return (
+    <>
+      {value ? <div>It's the first Yolo</div> : <SecondComponent />}
+      <button type='button' className='btn' onClick={() => setValue(!value)}>
+        Toggle State
+      </button>
+    </>
+  )
+}
+
+const SecondComponent = () => {
+  useEffect(() => {
+    console.log('2nnnnnnnddddd')
+  }, [])
+  return <></>
+}
+
+export default CleanupFunction
