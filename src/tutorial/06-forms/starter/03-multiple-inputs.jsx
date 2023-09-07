fecb9dab5b3f51ceca921491344069daaaab80cd
thenatural86 <philip.smalls86@gmail.com>
2023-09-07 12:26:38 -0400

    multiple inputs

--- src/tutorial/06-forms/starter/03-multiple-inputs.jsx
+++ src/tutorial/06-forms/starter/03-multiple-inputs.jsx
@@ -1,4 +1,16 @@
+import { useState } from 'react'
+
 const MultipleInputs = () => {
+  const [user, setUSer] = useState({
+    name: '',
+    email: '',
+    password: '',
+  })
+
+  const handleChange = (e) => {
+    setUSer({ ...user, [e.target.name]: e.target.value })
+  }
+
   return (
     <div>
       <form className='form'>
@@ -8,21 +20,42 @@ const MultipleInputs = () => {
           <label htmlFor='name' className='form-label'>
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input
+            type='text'
+            className='form-input'
+            id='name'
+            value={user.name}
+            onChange={handleChange}
+            name='name'
+          />
         </div>
         {/* email */}
         <div className='form-row'>
           <label htmlFor='email' className='form-label'>
             Email
           </label>
-          <input type='email' className='form-input' id='email' />
+          <input
+            type='email'
+            className='form-input'
+            id='email'
+            value={user.email}
+            onChange={handleChange}
+            name='email'
+          />
         </div>
         {/* email */}
         <div className='form-row'>
           <label htmlFor='password' className='form-label'>
             Password
           </label>
-          <input type='password' className='form-input' id='password' />
+          <input
+            type='password'
+            className='form-input'
+            id='password'
+            value={user.password}
+            onChange={handleChange}
+            name='password'
+          />
         </div>
 
         <button type='submit' className='btn btn-block'>
@@ -30,6 +63,6 @@ const MultipleInputs = () => {
         </button>
       </form>
     </div>
-  );
-};
-export default MultipleInputs;
+  )
+}
+export default MultipleInputs
