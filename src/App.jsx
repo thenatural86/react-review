40f5c712a6e0d6653a59be686e08feb58bec3a86
thenatural86 <philip.smalls86@gmail.com>
2023-09-07 14:31:55 -0400

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/06-forms/starter/04-other-inputs'
+import Starter from './tutorial/07-useRef/starter/01-useRef-basics'
 
 function App() {
   return (
--- src/tutorial/07-useRef/starter/01-useRef-basics.jsx
+++ src/tutorial/07-useRef/starter/01-useRef-basics.jsx
@@ -1,11 +1,14 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react'
 
 const UseRefBasics = () => {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState(0)
+  const refContainer = useRef(null)
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+    e.preventDefault()
+    const name = refContainer.current.value
+    console.log(name)
+  }
 
   return (
     <div>
@@ -14,7 +17,12 @@ const UseRefBasics = () => {
           <label htmlFor='name' className='form-label'>
             Name
           </label>
-          <input type='text' id='name' className='form-input' />
+          <input
+            type='text'
+            id='name'
+            className='form-input'
+            ref={refContainer}
+          />
         </div>
         <button type='submit' className='btn btn-block'>
           submit
@@ -25,7 +33,7 @@ const UseRefBasics = () => {
         increase
       </button>
     </div>
-  );
-};
+  )
+}
 
-export default UseRefBasics;
+export default UseRefBasics
