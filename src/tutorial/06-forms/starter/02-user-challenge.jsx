21ff8ed774cc47cd7e29d015292ad74835c9b4ab
thenatural86 <philip.smalls86@gmail.com>
2023-09-07 11:36:53 -0400

    submit function

--- src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,21 +1,52 @@
+import { useState } from 'react'
+import { data } from '../../../data'
+
 const UserChallenge = () => {
+  const [name, setName] = useState('')
+  const [users, setUsers] = useState(data)
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    if (!name) return
+    e.target.value
+    const fakeId = Date.now()
+    const newUser = {
+      id: fakeId,
+      name,
+    }
+    const updatedUsers = [...users, newUser]
+    setUsers(updatedUsers)
+    setName('')
+  }
+
   return (
-    <div>
+    <div onSubmit={submitHandler}>
       <form className='form'>
         <h4>Add User</h4>
         <div className='form-row'>
           <label htmlFor='name' className='form-label'>
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input
+            type='text'
+            className='form-input'
+            id='name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
 
         <button type='submit' className='btn btn-block'>
           submit
         </button>
       </form>
-      {/* render users below */}
+      <h4>
+        {users.map((obj) => {
+          const { name, id } = obj
+          return <div key={id}>{name}</div>
+        })}
+      </h4>
     </div>
-  );
-};
-export default UserChallenge;
+  )
+}
+export default UserChallenge
